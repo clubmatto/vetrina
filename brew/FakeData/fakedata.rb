63a8ab720142e2d4e64f8f5e7eb37a6546bf01a1
lucapette <ciao@lucapette.me>
2026-06-04 09:29:51 +0200

    fix: replace deprecated brews with custom Homebrew formula script
    - Remove brews section from goreleaser.yml
    - Add update-formula.sh script that generates a pre-compiled binary formula
    - Rewrite fakedata.rb for pre-compiled binaries (no Go dep needed)
    - Add post-release step in workflow to commit updated formula
    
    created with the help of deepseek

--- brew/FakeData/fakedata.rb
+++ brew/FakeData/fakedata.rb
@@ -2,19 +2,28 @@ class Fakedata < Formula
   desc "CLI tool to generate fake data rows for testing and development"
   homepage "https://matto.club/vetrina/fakedata"
   license "MIT"
-  head "https://github.com/clubmatto/vetrina.git", branch: "main"
+  version "VERSION"
 
-  stable do
-    url "https://github.com/clubmatto/vetrina/releases/download/fakedata/vVERSION/fakedata_VERSION_darwin_amd64.tar.gz"
-    sha256 "SHA256"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/clubmatto/vetrina/releases/download/fakedata/vVERSION/fakedata_VERSION_darwin_amd64.tar.gz"
+      sha256 "MAC_AMD64_SHA256"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/clubmatto/vetrina/releases/download/fakedata/vVERSION/fakedata_VERSION_darwin_arm64.tar.gz"
+      sha256 "MAC_ARM64_SHA256"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/clubmatto/vetrina/releases/download/fakedata/vVERSION/fakedata_VERSION_linux_amd64.tar.gz"
+      sha256 "LINUX_AMD64_SHA256"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/clubmatto/vetrina/releases/download/fakedata/vVERSION/fakedata_VERSION_linux_arm64.tar.gz"
+      sha256 "LINUX_ARM64_SHA256"
+    end
   end
 
-  depends_on "go" => :build
-
   def install
-    cd "fakedata" do
-      system "go", "build", "-ldflags", "-s -w -X main.version=#{version}", "-o", bin/"fakedata"
-    end
+    bin.install "fakedata"
   end
 
   test do
